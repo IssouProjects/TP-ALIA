641588c8a4d2e1952ca1b3c2e48bd523d538b623
Victor <vb4007@gmail.com>
2017-11-21 16:30:44 +0100

    Affichage ok
    Merci au merveilleux @TimotheeDurand

--- src/quatro.pl
+++ src/quatro.pl
@@ -30,7 +30,7 @@ initPieces(RemainingPieces) :-
     Piece13 = [1, 1, 0, 0],
     Piece14 = [1, 1, 0, 1],
     Piece15 = [1, 1, 1, 0],
-    Piece16 = [1, 1, 1, 1];
+    Piece16 = [1, 1, 1, 1],
     RemainingPieces = [Piece1, Piece2, Piece3, Piece4, Piece5, Piece6, Piece7, Piece8, Piece9, Piece10, Piece11, Piece12, Piece13, Piece14, Piece15, Piece16].
 
 
@@ -48,7 +48,7 @@ play(Player, BoardSize, BoardShape, BoardHole, BoardColor, RemainingPieces):-
     play(NextPlayer, BoardSize, BoardShape, BoardHole, BoardColor, RemainingPieces). % next turn!
 
 
-iaChoosePiece(BoardSize,BoardShape,BoardHole,BoardColor,Piece,Player,RemainingPieces) :- repeat, length(RemainingPieces, NumberPieces), random(1, NumberPieces, IndexPiece), nth0(IndexPiece, RemainingPieces, Piece), var(Piece), writeln(nth0(IndexPiece, RemainingPieces, Piece)).
+iaChoosePiece(BoardSize,BoardShape,BoardHole,BoardColor,Piece,Player,RemainingPieces) :- repeat, length(RemainingPieces, NumberPieces), random(1, NumberPieces, IndexPiece), nth0(IndexPiece, RemainingPieces, Piece).
 
 changePlayer(0,1).
 changePlayer(1,0).
@@ -58,21 +58,25 @@ gameover(BoardSize, BoardShape, BoardHole, BoardColor, Player) :- win(BoardSize,
 append([],L,L).
 append([H|T],L,[H|B]) :- append(T,L,B).
 
-playMoveBoard(Board,Move,NewBoard,Player) :- nth0(Move,NewBoard,Player), Board=NewBoard.
+playMoveBoard(Board,Move,Piece,NewBoard) :- Board=NewBoard, nth0(Move,NewBoard,Piece).
 
-iaChooseMove(BoardSize, BoardShape, BoardHole, BoardColor, Piece, Move, Player) :- repeat, Index is random(16), nth0(Index, BoardSize, Elem), var(Elem), !.
+iaChooseMove(BoardSize, BoardShape, BoardHole, BoardColor, Piece, Move, Player) :- repeat, Index is random(16), nth0(Index, BoardSize, Elem), var(Elem), Move is Index.
 
 playMove(BoardSize, BoardShape, BoardHole, BoardColor, Move, Piece, NewBoardSize, NewBoardShape, NewBoardHole, NewBoardColor) :-
-  playMoveBoard(BoardSize,Move,Piece,NewBoardSize), % Play the move and get the result in a new Board
-  playMoveBoard(BoardShape,Move,Piece,NewBoardShape), % Play the move and get the result in a new Board
-  playMoveBoard(BoardHole,Move,Piece,NewBoardHole), % Play the move and get the result in a new Board
-  playMoveBoard(BoardColor,Move,Piece,NewBoardColor). % Play the move and get the result in a new Board
+  nth0(0, Piece, Size),
+  nth0(1, Piece, Shape),
+  nth0(2, Piece, Hole),
+  nth0(3, Piece, Color),
+  playMoveBoard(BoardSize,Move,Size,NewBoardSize), % Play the move and get the result in a new Board
+  playMoveBoard(BoardShape,Move,Shape,NewBoardShape), % Play the move and get the result in a new Board
+  playMoveBoard(BoardHole,Move,Hole,NewBoardHole), % Play the move and get the result in a new Board
+  playMoveBoard(BoardColor,Move,Color,NewBoardColor). % Play the move and get the result in a new Board
 
 applyEntireMove(BoardSize, BoardShape, BoardHole, BoardColor, NewBoardSize, NewBoardShape, NewBoardHole, NewBoardColor) :- % Remove the old board from the KB and store the new one
   retract(boardSize(BoardSize)), assert(boardSize(NewBoardSize)), % Remove the old board from the KB and store the new one
-  retract(boardSize(BoardShape)), assert(boardSize(NewBoardShape)), % Remove the old board from the KB and store the new one
-  retract(boardSize(BoardHole)), assert(boardSize(NewBoardHole)), % Remove the old board from the KB and store the new one
-  retract(boardSize(BoardColor)), assert(boardSize(NewBoardColor)). % Remove the old board from the KB and store the new one
+  retract(boardShape(BoardShape)), assert(boardShape(NewBoardShape)), % Remove the old board from the KB and store the new one
+  retract(boardHole(BoardHole)), assert(boardHole(NewBoardHole)), % Remove the old board from the KB and store the new one
+  retract(boardColor(BoardColor)), assert(boardColor(NewBoardColor)). % Remove the old board from the KB and store the new one
 
 % Check if the game is won
 win(BoardSize, BoardShape, BoardHole, BoardColor):- winBoard(BoardSize); winBoard(BoardHole); winBoard(BoardShape); winBoard(BoardColor).
@@ -91,66 +95,66 @@ boardFull(Board) :- isBoardFull([]).
 isBoardFull([H|T]):- nonvar(H), isBoardFull(T).
 
 % Display the board
-displayBoard(BoardSize, BoardHole, BoardFull, BoardColor) :- write("========================="),write('\n'),
+displayBoard(BoardSize, BoardShape, BoardHole, BoardColor) :- write("========================="),write('\n'),
                 % first line
-                write("| "), printval(BoardSize, 0, 's', 'L'), write(' '), printval(BoardHole, 0, 'f', 'e'),write(" | "),
-                write(" | "), printval(BoardSize, 1, 's', 'L'), write(' '), printval(BoardHole, 1, 'f', 'e'), write(" | "),
-                write(" | "), printval(BoardSize, 2, 's', 'L'), write(' '), printval(BoardHole, 2, 'f', 'e'), write(" | "),
+                write("| "), printval(BoardSize, 0, 's', 'L'), write(' '), printval(BoardHole, 0, 'f', 'e'),
+                write(" | "), printval(BoardSize, 1, 's', 'L'), write(' '), printval(BoardHole, 1, 'f', 'e'),
+                write(" | "), printval(BoardSize, 2, 's', 'L'), write(' '), printval(BoardHole, 2, 'f', 'e'), 
                 write(" | "), printval(BoardSize, 3, 's', 'L'), write(' '), printval(BoardHole, 3, 'f', 'e'), write(" |"), write('\n'),
 
                 %second line
-                write("| "), printval(BoardShape, 0, 'o', 'q'), write(' '), printval(BoardColor, 0, 'w', 'b'),write(" | "),
-                write(" | "), printval(BoardShape, 1, 'o', 'q'), write(' '), printval(BoardColor, 1, 'w', 'b'), write(" | "),
-                write(" | "), printval(BoardShape, 2, 'o', 'q'), write(' '), printval(BoardColor, 2, 'w', 'b'), write(" | "),
+                write("| "), printval(BoardShape, 0, 'o', 'q'), write(' '), printval(BoardColor, 0, 'w', 'b'),
+                write(" | "), printval(BoardShape, 1, 'o', 'q'), write(' '), printval(BoardColor, 1, 'w', 'b'),
+                write(" | "), printval(BoardShape, 2, 'o', 'q'), write(' '), printval(BoardColor, 2, 'w', 'b'),
                 write(" | "), printval(BoardShape, 3, 'o', 'q'), write(' '), printval(BoardColor, 3, 'w', 'b'), write(" |"), write('\n'),
 
                 write("|=====|=====|=====|=====|\n"),
 
                 % first line
-                write("| "), printval(BoardSize, 4, 's', 'L'), write(' '), printval(BoardHole, 4, 'f', 'e'),write(" | "),
-                write(" | "), printval(BoardSize, 5, 's', 'L'), write(' '), printval(BoardHole, 5, 'f', 'e'), write(" | "),
-                write(" | "), printval(BoardSize, 6, 's', 'L'), write(' '), printval(BoardHole, 6, 'f', 'e'), write(" | "),
+                write("| "), printval(BoardSize, 4, 's', 'L'), write(' '), printval(BoardHole, 4, 'f', 'e'),
+                write(" | "), printval(BoardSize, 4, 's', 'L'), write(' '), printval(BoardHole, 5, 'f', 'e'),
+                write(" | "), printval(BoardSize, 6, 's', 'L'), write(' '), printval(BoardHole, 6, 'f', 'e'),
                 write(" | "), printval(BoardSize, 7, 's', 'L'), write(' '), printval(BoardHole, 7, 'f', 'e'), write(" |"), write('\n'),
 
                 %second line
-                write("| "), printval(BoardShape, 4, 'o', 'q'), write(' '), printval(BoardColor, 4, 'w', 'b'),write(" | "),
-                write(" | "), printval(BoardShape, 5, 'o', 'q'), write(' '), printval(BoardColor, 5, 'w', 'b'), write(" | "),
-                write(" | "), printval(BoardShape, 6, 'o', 'q'), write(' '), printval(BoardColor, 6, 'w', 'b'), write(" | "),
+                write("| "), printval(BoardShape, 4, 'o', 'q'), write(' '), printval(BoardColor, 4, 'w', 'b'),
+                write(" | "), printval(BoardShape, 5, 'o', 'q'), write(' '), printval(BoardColor, 5, 'w', 'b'),
+                write(" | "), printval(BoardShape, 6, 'o', 'q'), write(' '), printval(BoardColor, 6, 'w', 'b'),
                 write(" | "), printval(BoardShape, 7, 'o', 'q'), write(' '), printval(BoardColor, 7, 'w', 'b'), write(" |"), write('\n'),
 
                 write("|=====|=====|=====|=====|\n"),
 
                 % first line
-                write("| "), printval(BoardSize, 8, 's', 'L'), write(' '), printval(BoardHole, 8, 'f', 'e'),write(" | "),
-                write(" | "), printval(BoardSize, 9, 's', 'L'), write(' '), printval(BoardHole, 9, 'f', 'e'), write(" | "),
-                write(" | "), printval(BoardSize, 10, 's', 'L'), write(' '), printval(BoardHole, 10, 'f', 'e'), write(" | "),
+                write("| "), printval(BoardSize, 8, 's', 'L'), write(' '), printval(BoardHole, 8, 'f', 'e'),
+                write(" | "), printval(BoardSize, 9, 's', 'L'), write(' '), printval(BoardHole, 9, 'f', 'e'),
+                write(" | "), printval(BoardSize, 10, 's', 'L'), write(' '), printval(BoardHole, 10, 'f', 'e'),
                 write(" | "), printval(BoardSize, 11, 's', 'L'), write(' '), printval(BoardHole, 11, 'f', 'e'), write(" |"), write('\n'),
 
                 %second line
-                write("| "), printval(BoardShape, 8, 'o', 'q'), write(' '), printval(BoardColor, 8, 'w', 'b'),write(" | "),
-                write(" | "), printval(BoardShape, 9, 'o', 'q'), write(' '), printval(BoardColor, 9, 'w', 'b'), write(" | "),
-                write(" | "), printval(BoardShape, 10, 'o', 'q'), write(' '), printval(BoardColor, 10, 'w', 'b'), write(" | "),
+                write("| "), printval(BoardShape, 8, 'o', 'q'), write(' '), printval(BoardColor, 8, 'w', 'b'),
+                write(" | "), printval(BoardShape, 9, 'o', 'q'), write(' '), printval(BoardColor, 9, 'w', 'b'),
+                write(" | "), printval(BoardShape, 10, 'o', 'q'), write(' '), printval(BoardColor, 10, 'w', 'b'),
                 write(" | "), printval(BoardShape, 11, 'o', 'q'), write(' '), printval(BoardColor, 11, 'w', 'b'), write(" |"), write('\n'),
 
                 write("|=====|=====|=====|=====|\n"),
 
                 % first line
-                write("| "), printval(BoardSize, 12, 's', 'L'), write(' '), printval(BoardHole, 12, 'f', 'e'),write(" | "),
-                write(" | "), printval(BoardSize, 13, 's', 'L'), write(' '), printval(BoardHole, 13, 'f', 'e'), write(" | "),
-                write(" | "), printval(BoardSize, 14, 's', 'L'), write(' '), printval(BoardHole, 14, 'f', 'e'), write(" | "),
+                write("| "), printval(BoardSize, 12, 's', 'L'), write(' '), printval(BoardHole, 12, 'f', 'e'),
+                write(" | "), printval(BoardSize, 13, 's', 'L'), write(' '), printval(BoardHole, 13, 'f', 'e'),
+                write(" | "), printval(BoardSize, 14, 's', 'L'), write(' '), printval(BoardHole, 14, 'f', 'e'),
                 write(" | "), printval(BoardSize, 15, 's', 'L'), write(' '), printval(BoardHole, 15, 'f', 'e'), write(" |"), write('\n'),
 
                 %second line
-                write("| "), printval(BoardShape, 12, 'o', 'q'), write(' '), printval(BoardColor, 12, 'w', 'b'),write(" | "),
-                write(" | "), printval(BoardShape, 13, 'o', 'q'), write(' '), printval(BoardColor, 13, 'w', 'b'), write(" | "),
-                write(" | "), printval(BoardShape, 14, 'o', 'q'), write(' '), printval(BoardColor, 14, 'w', 'b'), write(" | "),
+                write("| "), printval(BoardShape, 12, 'o', 'q'), write(' '), printval(BoardColor, 12, 'w', 'b'),
+                write(" | "), printval(BoardShape, 13, 'o', 'q'), write(' '), printval(BoardColor, 13, 'w', 'b'),
+                write(" | "), printval(BoardShape, 14, 'o', 'q'), write(' '), printval(BoardColor, 14, 'w', 'b'),
                 write(" | "), printval(BoardShape, 15, 'o', 'q'), write(' '), printval(BoardColor, 15, 'w', 'b'), write(" |"), write('\n'),
 
                 write("|=====|=====|=====|=====|"),write('\n').
 
 printval(Board,N,CharTrue, CharFalse) :- nth0(N,Board,Val), var(Val), write('?'),!.
-printval(Board,N,CharTrue, CharFalse) :- nth0(N,Board,Val), var(Val), Val is 1, write(CharTrue).
-printval(Board,N,CharTrue, CharFalse) :- nth0(N,Board,Val), var(Val), Val is 0, write(CharFalse).
+printval(Board,N,CharTrue, CharFalse) :- nth0(N,Board,Val), Val is 1, write(CharTrue).
+printval(Board,N,CharTrue, CharFalse) :- nth0(N,Board,Val), Val is 0, write(CharFalse).
 %printval(Board,N,CharTrue, CharFalse) :- nth0(N,Board,Val), var(Val), write('?'),!.
 
 % ========================╗
