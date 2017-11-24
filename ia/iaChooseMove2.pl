301f2cb45d5b6e01e09f7802dae7c76f1d624c79
Vincent Redouté <vincent.redoute@live.fr>
2017-11-24 14:39:43 +0100

    Si l'ia peut placer une pièce permettant de gagner la partie elle le fait, sinon random.

--- ia/iaChooseMove2.pl
+++ ia/iaChooseMove2.pl
@@ -5,19 +5,16 @@
 %testMoves(Piece, TempMove, Index) :- repeat, var(Elem), nth0(Index, BoardSize, Elem), var(Elem), TempMove is Index, .
 
 
-iaChooseMove2(Piece, Move) :- boardSize(BoardSize), testMoves(BoardSize, Piece, Move).
-iaChooseMove2(_, Move) :- writeln('here'), repeat, Index is random(16), boardSize(BoardSize), nth0(Index, BoardSize, Elem), var(Elem), Move is Index.
+iaChooseMove2(Piece, Move) :-testMoves(Piece, Move).
+iaChooseMove2(_, Move) :- writeln('Play a random move.'), repeat, Index is random(16), boardSize(BoardSize), nth0(Index, BoardSize, Elem), var(Elem), Move is Index.
 
 
-testMoves([],_,_):- writeln('here2').
-testMoves([X|B],Piece,Move) :-
+testMoves(Piece,Move) :-
+  between(0,15,X),
   nth0(X, BoardSize, Elem), var(Elem), Move is X,
   boardSize(BoardSize), boardShape(BoardShape), boardHole(BoardHole), boardColor(BoardColor),
   playMove(BoardSize, BoardShape, BoardHole, BoardColor, Move, Piece, NewBoardSize, NewBoardShape, NewBoardHole, NewBoardColor),
-  write('Move tested '), writeln(Move),
-  displayBoard(NewBoardSize, NewBoardShape, NewBoardHole, NewBoardColor),
-  winTest(NewBoardSize, NewBoardShape, NewBoardHole, NewBoardColor),
-  testMoves(B, Piece, Move).
+  winTest(NewBoardSize, NewBoardShape, NewBoardHole, NewBoardColor).
 
 % Check if the game is won
 winTest(NewBoardSize, NewBoardShape, NewBoardHole, NewBoardColor):- (winBoard(NewBoardSize); winBoard(NewBoardShape); winBoard(NewBoardHole); winBoard(NewBoardColor)).
