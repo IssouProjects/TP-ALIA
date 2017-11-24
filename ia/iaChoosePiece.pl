ee74542b6e246f4ebf0596e9e64e80382b94d635
Quentin JODER <quentinjoder@gmail.com>
2017-11-24 11:33:00 +0100

    It works... badly.

--- ia/iaChoosePiece.pl
+++ ia/iaChoosePiece.pl
@@ -2,21 +2,22 @@
 %:- consult(player).
 
 %Check if the other player can't win with this Piece
-cantWinWithThisPiece(_,0,_,_,_,_):-false.
-cantWinWithThisPiece(Piece, N, BoardSize, BoardShape, BoardHole, BoardColor):-
+winWithThisPiece(_,0,_,_,_,_):-false.
+winWithThisPiece(Piece, N, BoardSize, BoardShape, BoardHole, BoardColor):-
   M is N-1,
-  writeln(M),
   Move is M,
-  cantWinWithThisPiece(Piece, M, BoardSize, BoardShape, BoardHole, BoardColor),
   playMove(BoardSize, BoardShape, BoardHole, BoardColor, Move, Piece,
   NewBoardSize, NewBoardShape, NewBoardHole, NewBoardColor),
-  \+ win(NewBoardSize,NewBoardHole,NewBoardColor,NewBoardShape),
-  writeln("He can't win with this piece").
+  win(NewBoardSize,NewBoardHole,NewBoardColor,NewBoardShape).
 
 % IA pour le choix d'une piece pour l'adversaire
 iaChoosePiece(Piece,RemainingPieces, BoardSize, BoardShape, BoardHole, BoardColor) :-
-  nth0(_, RemainingPieces, Piece),
-  cantWinWithThisPiece(Piece, 16, BoardSize, BoardShape, BoardHole, BoardColor),
+  length(RemainingPieces, NumberPieces),
+  %0 =< N =<NumberPieces,
+  write("bang"),
+  nth0(N, RemainingPieces, Piece),
+  write("bong"),
+  \+winWithThisPiece(Piece, 16, BoardSize, BoardShape, BoardHole, BoardColor),
   !,
   supprime(Piece,RemainingPieces, NewRemainingPieces),
   retract(remainingPieces(RemainingPieces)),
