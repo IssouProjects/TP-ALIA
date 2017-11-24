7f6e15f423956b4b5efe4415158ea47a4ae2049d
Victor <vb4007@gmail.com>
2017-11-24 09:14:47 +0100

    Electron interface

---
+++ ui/renderer.js
@@ -0,0 +1,19 @@
+// ici on pourra utiliser les api node
+const swipl = require('swipl')
+
+/*
+swipl.call("consult('ia/test.pl')")
+
+const ret = swipl.call('ancetre3(X,Y).');
+
+if (ret) {
+  console.log(`${ret.X}`)
+  console.log(`${ret.Y}`)
+  document.writeln(`${ret.X}`)
+  document.writeln(`${ret.Y}`)
+} else {
+  console.log('Call failed.');
+}
+*/
+
+console.log('Hello you')
