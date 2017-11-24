7ea4b42f28dab130d5e40c4cbfb802fc302893b9
Quentin JODER <quentinjoder@gmail.com>
2017-11-24 14:14:13 +0100

    Changement Affichage
    Fait une méthode win(...) sans affichage pour ma méthode de choix de pièce. + une balise TODO

--- ia/end.pl
+++ ia/end.pl
@@ -2,11 +2,11 @@
 % Verification de la fin d'une partie
 %
 
-gameover() :- win(BoardSize,BoardHole,BoardColor,BoardShape), !.
+gameover() :- win(), !.
 gameover() :- boardShape(BoardShape), isBoardFull(BoardShape).
 
 % Check if the game is won
-win(BoardSize,BoardHole,BoardColor,BoardShape):- boardSize(BoardSize), boardShape(BoardShape), boardHole(BoardHole), boardColor(BoardColor), (winBoard(BoardSize); winBoard(BoardHole); winBoard(BoardShape); winBoard(BoardColor)).
+win():- boardSize(BoardSize), boardShape(BoardShape), boardHole(BoardHole), boardColor(BoardColor), (winBoard(BoardSize); winBoard(BoardHole); winBoard(BoardShape); winBoard(BoardColor)).
   winBoard(Board) :- Board = [P,Q,R,S,_,_,_,_,_,_,_,_,_,_,_,_], P==Q, Q==R, R==S, nonvar(P), write('first row completed. '). % first row
   winBoard(Board) :- Board = [_,_,_,_,P,Q,R,S,_,_,_,_,_,_,_,_], P==Q, Q==R, R==S, nonvar(P), write('second row completed. '). % second row
   winBoard(Board) :- Board = [_,_,_,_,_,_,_,_,P,Q,R,S,_,_,_,_], P==Q, Q==R, R==S, nonvar(P), write('third row completed. '). % third row
@@ -17,6 +17,18 @@ win(BoardSize,BoardHole,BoardColor,BoardShape):- boardSize(BoardSize), boardShap
   winBoard(Board) :- Board = [_,_,_,P,_,_,_,Q,_,_,_,R,_,_,_,S], P==Q, Q==R, R==S, nonvar(P), write('fourth column completed. '). % fourth column
   winBoard(Board) :- Board = [P,_,_,_,_,Q,_,_,_,_,R,_,_,_,_,S], P==Q, Q==R, R==S, nonvar(P), write('first diagonal completed. '). % first diagonal
   winBoard(Board) :- Board = [_,_,_,P,_,_,Q,_,_,R,_,_,S,_,_,_], P==Q, Q==R, R==S, nonvar(P), write('second diagonal completed. '). % second diagonal
+
+win(BoardSize,BoardHole,BoardColor,BoardShape):-(winBoard(BoardSize); winBoard(BoardHole); winBoard(BoardShape); winBoard(BoardColor)).
+  winBoard(Board) :- Board = [P,Q,R,S,_,_,_,_,_,_,_,_,_,_,_,_], P==Q, Q==R, R==S, nonvar(P). % first row
+  winBoard(Board) :- Board = [_,_,_,_,P,Q,R,S,_,_,_,_,_,_,_,_], P==Q, Q==R, R==S, nonvar(P). % second row
+  winBoard(Board) :- Board = [_,_,_,_,_,_,_,_,P,Q,R,S,_,_,_,_], P==Q, Q==R, R==S, nonvar(P). % third row
+  winBoard(Board) :- Board = [_,_,_,_,_,_,_,_,_,_,_,_,P,Q,R,S], P==Q, Q==R, R==S, nonvar(P). % fouth row
+  winBoard(Board) :- Board = [P,_,_,_,Q,_,_,_,R,_,_,_,S,_,_,_], P==Q, Q==R, R==S, nonvar(P). % first column
+  winBoard(Board) :- Board = [_,P,_,_,_,Q,_,_,_,R,_,_,_,S,_,_], P==Q, Q==R, R==S, nonvar(P). % second column
+  winBoard(Board) :- Board = [_,_,P,_,_,_,Q,_,_,_,R,_,_,_,S,_], P==Q, Q==R, R==S, nonvar(P). % third column
+  winBoard(Board) :- Board = [_,_,_,P,_,_,_,Q,_,_,_,R,_,_,_,S], P==Q, Q==R, R==S, nonvar(P). % fourth column
+  winBoard(Board) :- Board = [P,_,_,_,_,Q,_,_,_,_,R,_,_,_,_,S], P==Q, Q==R, R==S, nonvar(P). % first diagonal
+  winBoard(Board) :- Board = [_,_,_,P,_,_,Q,_,_,R,_,_,S,_,_,_], P==Q, Q==R, R==S, nonvar(P). % second diagonal
 
 isBoardFull([]).
 isBoardFull([H|T]):- nonvar(H), isBoardFull(T).
