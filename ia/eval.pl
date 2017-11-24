c771dca487ae4ec0513382dff99a00a975c25f24
Tim <durandtimothee@orange.fr>
2017-11-24 11:55:34 +0100

    correction

--- ia/eval.pl
+++ ia/eval.pl
@@ -1,6 +1,13 @@
 % récupérer toutes les lignes intéressantes
 %eval(Board, EvaluationValue).
 
+evalAllBoard(BoardSize, BoardShape, BoardHole, BoardColor, Value):-
+  evalBoard(BoardSize, S0),
+  evalBoard(BoardShape, S1),
+  evalBoard(BoardHole, S2),
+  evalBoard(BoardColor, S3),
+  Value is S0+S1+S2+S3.
+
 evalBoard(Board, Value):- Board = [X1,_,_,_,Y1,_,_,_,Z1,_,_,_,T1,_,_,_], evalLine([X1,Y1,Z1,T1], S1),
 Board = [_,X2,_,_,_,Y2,_,_,_,Z2,_,_,_,T2,_,_], evalLine([X2,Y2,Z2,T2], S2),
 Board = [_,_,X3,_,_,_,Y3,_,_,_,Z3,_,_,_,T3,_], evalLine([X3,Y3,Z3,T3], S3),
@@ -15,11 +22,8 @@ Board = [_,_,_,_,_,_,_,_,X9,Y9,Z9,T9,_,_,_,_], evalLine([X9,Y9,Z9,T9], S9),
 Board = [_,_,_,_,_,_,_,_,_,_,_,_,X10,Y10,Z10,T10], evalLine([X10,Y10,Z10,T10], S10),
 Value is S1+S2+S3+S4+S5+S6+S7+S8+S9+S10.
 
-
-init:- length(Line, 4), nth0(0,Line, 0), nth0(2,Line, 0), nth0(3,Line, 0), evalLine(Line, Value), write(Value).
-
 transformNulls(Line, Z):- transformNull(Line, Z, 0), transformNull(Line, Z, 1),transformNull(Line, Z, 2),transformNull(Line, Z, 3).
-transformNull(Line, _, N):- nth0(N,Line,X), var(X), X is -1.
+transformNull(Line, _, N):- nth0(N,Line,X), var(X).
 transformNull(Line, Z, N):- nth0(N,Line,X), nonvar(X), Z is X.
 
 evalLine(Line, Value):- transformNulls(Line, Z), var(Z), Value is 0, !.
@@ -29,4 +33,4 @@ evalLine(_, Value):- Value is 0.
 % calculate the number of similar pieces in the line
 evalNbSimi([], _, 0).
 evalNbSimi([X|Line], Z, NbSimi):- X == Z, evalNbSimi(Line, Z, NbSimi2), NbSimi is NbSimi2+1.
-evalNbSimi([X|Line], Z, NbSimi):- X == -1, evalNbSimi(Line, Z, NbSimi).
+evalNbSimi([X|Line], Z, NbSimi):- var(X), evalNbSimi(Line, Z, NbSimi).
