2996db32805bd1f5da5bcab537253ac2e84610ed
Victor <vb4007@gmail.com>
2017-11-28 17:47:35 +0100

    Enhance compare ui

--- ui/renderer.js
+++ ui/renderer.js
@@ -2,6 +2,7 @@ const prolog = require('./prolog');
 
 var winner0 = 0
 var winner1 = 0
+var ties = 0
 
 var stop = false
 
@@ -12,15 +13,19 @@ function play(games) {
     //winner = output.replace(/\n/g, "<br />");
     if(winner == 0) {
       winner0 ++
+      document.getElementById("winner0").style.width = winner0 + "%"
+      document.getElementById("wins0").innerHTML = winner0
     }
     else if(winner == 1) {
       winner1 ++
+      document.getElementById("winner1").style.width = winner1 + "%"
+      document.getElementById("wins1").innerHTML = winner1
     }
-
-    document.getElementById("winner0").style.width = winner0 + "%"
-    document.getElementById("winner1").style.width = winner1 + "%"
-    document.getElementById("wins0").innerHTML = winner0
-    document.getElementById("wins1").innerHTML = winner1
+    else {
+      ties ++
+      document.getElementById("ties").innerHTML = ties
+    }
+    
     if(winner0 + winner1 < games && stop === false) {
       play(games)
     } else {
@@ -38,6 +43,7 @@ function done() {
 document.getElementById("start").addEventListener("click", function () {
   winner0 = 0
   winner1 = 0
+  ties = 0
   document.getElementById("start").disabled = true
   document.getElementById("stop").disabled = false
   play(100)
