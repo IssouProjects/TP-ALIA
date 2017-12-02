4f55ee119a9f49f0031c603b4b8c62d097e91233
Tim <durandtimothee@orange.fr>
2017-12-02 23:54:59 +0100

    pushing the last atrocities

--- ia/minimax2.pl
+++ ia/minimax2.pl
@@ -1,42 +1,51 @@
 iaChooseMoveMinimax(SelectedPiece, BestMove, Depth):-
 	boardShape(BoardShape), boardSize(BoardSize), boardHole(BoardHole), boardColor(BoardColor),
- 	minimax(BoardShape, BoardSize, BoardColor, BoardHole, SelectedPiece, BestMove, _, Depth).
+	remainingPieces(RemainingPieces),
+ 	minimax(BoardShape, BoardSize, BoardColor, BoardHole, RemainingPieces, SelectedPiece, BestMove, _, Depth).
 
-
-
-minimax(BoardShape, BoardSize, BoardColor, BoardHole, Piece, NextMove, Eval, Depth) :-
+minimax(BoardShape, BoardSize, BoardColor, BoardHole,RemainingPieces, Piece, NextMove, Eval, Depth) :-
 	Depth < 5,
 	NewDepth is Depth + 1,
-	list_available_moves(BoardSize, ListMoves),
-	best(BoardShape, BoardSize, BoardColor, BoardHole, Piece, ListMoves, NextMove, Eval, NewDepth), !.
+	list_available_moves(BoardSize, ListMoves, RemainingPieces),
+	best(BoardShape, BoardSize, BoardColor, BoardHole,RemainingPieces, Piece, ListMoves, NextMove, Eval, NewDepth), !.
 
-minimax(BoardShape, BoardSize, BoardColor, BoardHole,_ , _, Eval, _) :-
+minimax(BoardShape, BoardSize, BoardColor, BoardHole,_ ,_ , _, Eval, _) :-
   evalAllBoard(BoardSize, BoardShape, BoardHole, BoardColor, Eval),
 	displayBoard(BoardSize, BoardShape, BoardHole, BoardColor), writeln(Eval),!.
 
-best(BoardShape, BoardSize, BoardColor, BoardHole, Piece, [Move], Move, Eval, Depth) :-
-  playMove(BoardSize, BoardShape, BoardHole, BoardColor, Move, Piece, NewBoardSize, NewBoardShape, NewBoardHole, NewBoardColor),
-  minimax(NewBoardSize, NewBoardShape, NewBoardColor, NewBoardHole ,Piece,  _, Eval, Depth), !.
+best(BoardShape, BoardSize, BoardColor, BoardHole,RemainingPieces, Piece, [[Move|[NextPiece]]], [Move|[NextPiece]], Eval, Depth) :-
+	playMove(BoardSize, BoardShape, BoardHole, BoardColor, Move, Piece, NewBoardSize, NewBoardShape, NewBoardHole, NewBoardColor),
+	supprime(NextPiece, RemainingPieces, NewRemainingPieces),
+  minimax(NewBoardSize, NewBoardShape, NewBoardColor, NewBoardHole,NewRemainingPieces, NextPiece, _, Eval, Depth), !.
 
-best(BoardShape, BoardSize, BoardColor, BoardHole, Piece, [Move|Moves], BestMove, BestEval, Depth) :-
+best(BoardShape, BoardSize, BoardColor, BoardHole,RemainingPieces, Piece, [[Move|[NextPiece]]|Moves], BestMove, BestEval, Depth) :-
   dechain(Move, Move1),
   playMove(BoardSize, BoardShape, BoardHole, BoardColor, Move, Piece, NewBoardSize, NewBoardShape, NewBoardHole, NewBoardColor),
-  minimax(Piece, NewBoardSize, NewBoardShape, NewBoardHole, NewBoardColor, _, Eval, Depth),
-  best(BoardShape, BoardSize, BoardColor, BoardHole, Piece, Moves, BestMove1, BestEval1, Depth),
-  better_of(Piece, Move1, Eval, BestMove1, BestEval1, BestMove, BestEval, Depth).
+	supprime(NextPiece, RemainingPieces, NewRemainingPieces),
+  minimax(NewBoardSize, NewBoardShape, NewBoardColor, NewBoardHole,NewRemainingPieces, NextPiece, _, Eval, Depth),
+  best(BoardShape, BoardSize, BoardColor, BoardHole,NewRemainingPieces, NextPiece, Moves, BestMove1, BestEval1, Depth),
+  better_of(Move1, Eval, BestMove1, BestEval1, BestMove, BestEval, Depth).
 
-list_available_moves(BoardSize, FinalListMoves):- list_available_moves(BoardSize, [],FinalListMoves, 0).
-list_available_moves(_,FinalListMoves, FinalListMoves, 16).
-list_available_moves(BoardSize, ListMoves, FinalListMoves, I):-
+% HELP MEEEEEEE
+list_available_moves(BoardSize, FinalListMoves, RemainingPieces):- list_available_moves(BoardSize, [], RemainingPieces, FinalListMoves, 0).
+list_available_moves(_,FinalListMoves, _, FinalListMoves, 16).
+list_available_moves(BoardSize, ListMoves,RemainingPieces,FinalListMoves, I):-
 	N is I+1,
 	nth0(I, BoardSize, Elem), var(Elem),
-	append(ListMoves,[I],NewListMoves),
-	list_available_moves(BoardSize, NewListMoves, FinalListMoves, N).
+	appendAllPieces(I,RemainingPieces, ListMoves,NewListMoves),
+	list_available_moves(BoardSize, NewListMoves, RemainingPieces, FinalListMoves, N).
 
-list_available_moves(BoardSize, ListMoves,FinalListMoves, I):-
+list_available_moves(BoardSize, ListMoves,RemainingPieces,FinalListMoves, I):-
 	N is I+1,
 	nth0(I, BoardSize, Elem), nonvar(Elem),
-	list_available_moves(BoardSize, ListMoves,FinalListMoves, N).
+	list_available_moves(BoardSize, ListMoves,RemainingPieces,FinalListMoves, N).
+
+appendAllPieces(_, [], ListMovesComplete, ListMovesComplete).
+appendAllPieces(I, [Piece|RemainingPieces], ListMoves, ListMovesComplete):-
+	append(ListMoves,[[I, Piece]],NewListMoves),
+	appendAllPieces(I, RemainingPieces, NewListMoves, ListMovesComplete).
+
+
 
 dechain([Move],Move).
 dechain([_|Moves],Last) :- last(Moves, Last).
